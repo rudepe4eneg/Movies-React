a091ef6349f827c8a0ae95cad4b5386167e247cf
Daniil Zaytsev <daniil_zaytsev_02@mail.ru>
2023-10-13 14:27:14 +0300

    key error done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,7 +12,7 @@ export class Main extends Component {
     };
 
     componentDidMount() {
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
