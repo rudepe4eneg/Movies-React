18240427b30c6ed2b6b9ffd411e66bf67514869a
Daniil Zaytsev <daniil_zaytsev_02@mail.ru>
2023-10-13 14:36:50 +0300

    https done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,7 +12,7 @@ export class Main extends Component {
     };
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
